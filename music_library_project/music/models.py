f7c4316991c2035fb83eca6ecedf38a420a72e12
unknown <schmidt1519@gmail.com>
2021-06-18 14:15:33 -0500

    Updated Song model to include genre and changed release date to DateField. Updated SongSerializers fields to include genre.

--- music_library_project/music/models.py
+++ music_library_project/music/models.py
@@ -7,5 +7,5 @@ class Song(models.Model):
     artist = models.CharField(max_length=50)
     album = models.CharField(max_length=50)
     genre = models.CharField(max_length=50)
-    release_date = models.DateTimeField(blank=True, null=True)
+    release_date = models.DateField()
     likes = models.IntegerField(default=0)
